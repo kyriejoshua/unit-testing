af62d6ab77719bf322594e64ce03341637ecdd99
zhengyuan zhu <zhuzy@broada.com>
2018-07-08 20:41:04 +0800

    feat: add promise and so on

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-export default function sum(a, b) {
+export function sum(a, b) {
 	return a + b
+}
+
+export function fetchData(callback) {
+  const data = 'async data'
+  callback(data)
+}
+
+export function handleData(data) {
+  return new Promise((resolve, reject) => {
+    window.setTimeout(() => resolve({ res: data.res + 1 }, 3000))
+  })
+}
+
+export function handleDataReject(data) {
+  return new Promise((resolve, reject) => {
+    window.setTimeout(() => {
+      if (Object.prototype.toString.call(data) !== '[object Object]') {
+        reject('Type Error!!!')
+      }
+      resolve({ res: data.res + 1})
+    }, 3000)
+  })
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import sum from './index'
+import { sum, fetchData, handleData, handleDataReject } from './index'
 
 test('1 + 2 equals to 3', () => {
 	expect(sum(1, 2)).toBe(3)
@@ -7,4 +7,25 @@ test('1 + 2 equals to 3', () => {
 test('比较对象相等', () => {
 	const data = { a: 1, b: '2' }
 	expect(data).toEqual({ a: 1, b: '2' })
+})
+
+test('测试回调', (done) => {
+  function callback(data) {
+    expect(data).toBe('async data')
+    done()
+  }
+  fetchData(callback)
+})
+
+test('测试异步代码', () => {
+  return handleData({ res: 1 }).then((data) => {
+    expect(data).toEqual({ res: 2 })
+  })
+})
+
+test('测试 Promise reject 代码', () => {
+  expect.assertions(1)
+  return handleDataReject('test rejected').catch((data) => {
+    expect(data).toMatch('Type Error!!!')
+  })
 })
